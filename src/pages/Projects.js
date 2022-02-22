39141e7dfafb6ea818ed75bcf228fcd4aadbeb8a
eduardtomasek <tomasek.e@gmail.com>
2022-02-22 08:11:26 +0100

    projects grid sketch

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import Grid from '@mui/material/Grid'
+
+import ProjectCard from '../components/ProjectCard'
+
 const ProjectsPage = () => {
-    return <div> This is the Project bitch.</div>
+    return <div>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {Array.from(Array(40)).map(index => (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                    <ProjectCard />
+                </Grid>
+            ))}
+        </Grid>
+    </div>
 }
 
 export default ProjectsPage
